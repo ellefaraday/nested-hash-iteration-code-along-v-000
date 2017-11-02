890c900670b668c68a1d10941c1c712a1e4d6431
Elle Faraday <faraday.elle@gmail.com>
2017-11-02 20:49:01 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,9 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |contact, attributes_hash|
     attributes_hash.each do |attribute, details|
-      binding.pry
+    
       if attribute == :favorite_ice_cream_flavors
+          binding.pry
         if details.include?("strawberry")
           detailes.delete("strawberry")
         end
